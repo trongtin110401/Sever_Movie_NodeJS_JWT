3fdaa7ed87d8db2751db9c98165448c67482a838
trong tin <trongtin110401@gmail.com>
2023-01-19 13:27:12 +0700

    a

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+let routes = require('./src/route/api') //importing route
+routes(app)
+
+app.use(function(req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found'})
+})
+
+app.listen(port)
+
+console.log('RESTful API server started on: ' + port)
